0e61d383983d193affaaa98faa61a8257bf45205
renan-dell\NovaComp <rrodrigues@crnova.com>
2022-06-27 19:58:27 -0300

    compile

---
+++ lib_my_string.h
@@ -0,0 +1,13 @@
+#define STRUCT(NAME)                    \
+    typedef struct _##NAME NAME##_t;    \
+    typedef NAME##_t *NAME##_p;         \
+    struct _##NAME
+
+STRUCT(string)
+{
+    char len;
+    char arr[0];
+};
+
+void string_display(string_p str);
+int string_cmp(string_p str1, char len, char arr[]);
