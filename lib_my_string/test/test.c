9c79b14f1b4d7060f30931dcf0ea466099b4b1a3
Renan Souza <renan.rodrigues.souza@usp.br>
2022-07-27 10:16:55 -0300

    Tests (#6)
    * test
    
    * mid commit
    
    * mid commit
    
    * add test
    
    * mid commit
    
    * mid commit
    
    * mid commit
    
    * test faillure
    
    * correct test
    
    * mid commit
    
    Co-authored-by: renan-dell\NovaComp <rrodrigues@crnova.com>

---
+++ lib_my_string/test/test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "../code.c"
+
+int test_string_cmp()
+{
+    int len = 3;
+    string_p str = malloc(sizeof(string_t) + 3);
+    str->len = 3;
+    str->arr[0] = 1;
+    str->arr[1] = 1;
+    str->arr[2] = 1;
+
+    char arr[3] = {1, 1, 1};
+
+    for(int i=0; i<3; i++)
+    {
+        arr[i] = 0;
+        int res = string_cmp(str, arr);
+        arr[i] = 1;
+        
+        assert(res == i);
+    }
+}
+
+int main() {
+    setbuf(stdout, NULL);
+    test_string_cmp();
+    printf("Test successful\n");
+    return 0;
+}
