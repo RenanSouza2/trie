aa8049034d09a3ac88f315351481e7fb94a86e1e
renan-dell\NovaComp <rrodrigues@crnova.com>
2022-07-31 19:18:08 -0300

    mid commit

--- lib/lib_memory_trie/code.c
+++ lib/lib_memory_trie/code.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "../lib_trie_base/header.h"
 #include "../utils/header.h"
@@ -6,36 +7,47 @@
 // STRUCT(trie_info)
 // {
 //     int max, len;
-//     int pointer_size;
+//     int pointer_size, fork_size;
 //     trie_pointer_p null;
 //
 //     trie_pointer                get_trie;
+//     pointer_trie                get_pointer;
 //     int_pointer                 pointer_is_null;
 //     void_pointer                pointer_display;
 //     pointer_pointer_int_pointer trie_fork_connect;
 //     pointer_pointer_2           trie_path_connect;
 //     pointer_pointer_int         trie_fork_disconnect;
-//     pointer_pointer_int         trie_fork_create;
-//     pointer_char_charv_pointer  trie_path_create_force;
-//     pointer_char_charv_pointer  trie_path_create;
-//     pointer_value               trie_leaf_create;
+//     pointer_pointer_value       trie_leaf_set_value;
 //     void_pointer                trie_free_single;
 //     void_pointer                trie_free;
 //     int_trie                    trie_fork_first_key;
-//     void_pointer                pointer_free;
 //
-//     value_info_t vi;
+//     value_info_p vi;
 // };
+
+#define MTP(POINTER) ((mem_trie_fork_p)(POINTER))
+
+#define MAX 10
+
+STRUCT(mem_trie_fork)
+{
+    trie_fork_t t;
+    trie_pointer_p next[MAX];
+};
 
 trie_p mem_get_trie(trie_pointer_p tp)
 {
-    return *(trie_p*)tp;
+    return T(tp);
+}
+
+trie_pointer_p mem_get_pointer(trie_p t)
+{
+    return (trie_pointer_p)t;
 }
 
 int mem_pointer_is_null(trie_pointer_p tp)
 {
-    trie_p t = mem_get_trie(tp);
-    return t == NULL;
+    return tp == NULL;
 }
 
 void mem_trie_pointer_display(trie_pointer_p tp)
@@ -44,9 +56,36 @@ void mem_trie_pointer_display(trie_pointer_p tp)
     mem_pointer_display(t);
 }
 
-//     trie_fork_connect;
+trie_pointer_p mem_trie_fork_disconnect(trie_pointer_p tp, int key)
+{
+    if(MTP(tp)->next[key] == NULL) return tp;
+
+    if(TF(tp)->connected == 1)
+    {
+        free(tp);
+        return NULL;
+    }
+
+    (TF(tp)->connected)--;
+    return tp;
+}
+
+trie_pointer_p mem_trie_fork_connect(trie_pointer_p tp, int key, trie_pointer_p tp_next)
+{
+    if(tp_next == NULL) return mem_trie_fork_disconnect(tp, key);
+
+    if(MTP(tp)->next[key] == NULL) (TF(tp)->connected)++;
+
+    MTP(tp)->next[key] = tp_next;
+    return tp;
+}
+
+trie_pointer_p mem_trie_path_connect(trie_pointer_p tp, trie_pointer_p tp_next)
+{
+    
+}
+
 //     trie_path_connect;
-//     trie_fork_disconnect;
 //     trie_fork_create;
 //     trie_path_create_force;
 //     trie_path_create;
