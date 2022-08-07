82d89cf3e748d262263825e316e17f2a861ecbae
renan-dell\NovaComp <rrodrigues@crnova.com>
2022-08-07 04:22:16 -0300

    all tests

--- lib/lib_base_trie/test/test.c
+++ lib/lib_base_trie/test/test.c
@@ -508,33 +508,36 @@ void test_delete_1()
     assert(tp == NULL);
 }
 
-// void test_delete_2()
-// {
-//     printf("\n\ttest_delete_2");
-//     value_info_p vi = get_int_value_info();
-//     char arr[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-//
-//      trie_p t = NULL;
-//     value_p value = set_int(1);
-//     trie_insert(vi, &t, arr, value);
-//
-//     arr[5] = 1;
-//     value = set_int(2);
-//     trie_insert(vi, &t, arr, value);
-//
-//     arr[7] = 1;
-//     value = set_int(3);
-//     trie_insert(vi, &t, arr, value);
-//     trie_delete(vi, &t, arr);
-//
-//     assert_path(T(t), NULL, 5, arr);
-//     trie_p t1 = TP(t)->next;
-//     assert_fork(t1, 1, NULL);
-//     t1 = TF(t1)->next[1];
-//     assert_path(t1, NULL, 2, arr);
-//
-//      trie_free(t);
-// }
+void test_delete_2()
+{
+    printf("\n\ttest_delete_2");
+    char arr[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    pointer_p tp = NULL;
+    value_p value = set_int(1);
+    trie_insert(ti, &tp, arr, value);
+
+    arr[5] = 1;
+    value = set_int(2);
+    trie_insert(ti, &tp, arr, value);
+
+    arr[7] = 1;
+    value = set_int(3);
+    trie_insert(ti, &tp, arr, value);
+    trie_delete(ti, &tp, arr);
+
+    assert_path(tp, 0, 5, arr);
+
+    trie_p t = PI->get(tp);
+    pointer_p tp_aux = PN(t);
+    assert_fork(tp_aux, 1, 0);
+
+    t = PI->get(tp_aux);
+    tp_aux = FN(t, 1);
+    assert_path(tp_aux, 0, 2, arr);
+
+    trie_free(ti, tp);
+}
 
 void test_integration()
 {
@@ -543,7 +546,7 @@ void test_integration()
     test_querie();
 
     test_delete_1();
-    // test_delete_2();
+    test_delete_2();
 
 }
 
